4360091adf4454245aa165575dec273c0deb886e
Radyslav Krasnov <radislavkrasnoff@gmail.com>
2024-07-21 19:59:54 +0300

    modify graph creation

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# goit-algo-hw-06
+```
+Vertices quantity in the graph: 33
+
+Edges quantity in the graph: 68
+
+Vertex Paris has degree 16
+Vertex Lyon has degree 7
+Vertex Marseille has degree 4
+Vertex Montpellier has degree 4
+Vertex Barcelona has degree 1
+Vertex Nice has degree 1
+Vertex Grenoble has degree 4
+Vertex Saint-Etienne has degree 1
+Vertex Milano has degree 1
+Vertex Geneve has degree 3
+Vertex Zurich has degree 3
+Vertex Reims has degree 7
+Vertex Luxemburg has degree 6
+Vertex Nancy has degree 6
+Vertex Frankfurt has degree 6
+Vertex Stuttgart has degree 7
+Vertex Strasbourge has degree 8
+Vertex Colmar has degree 1
+Vertex Lille has degree 3
+Vertex Bruxelles has degree 4
+Vertex Amsterdam has degree 1
+Vertex Rennes has degree 7
+Vertex Saint-Malo has degree 2
+Vertex Brest has degree 2
+Vertex Quimper has degree 1
+Vertex Nantes has degree 4
+Vertex Bordeaux has degree 7
+Vertex Toulouse has degree 3
+Vertex Pau has degree 3
+Vertex Hendaye has degree 3
+Vertex London has degree 3
+Vertex La Rochelle has degree 5
+Vertex NancyFrankfurt has degree 2
+```
+![alt text](./tgv_graph.png)
--- tgv_graph.py
+++ tgv_graph.py
@@ -1,190 +1,210 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-tgv_graph = {
-    'Paris': [
-        'London', 
-        'Bruxelles', 
-        'Nantes', 
-        'Rennes', 
-        'La Rochelle', 
-        'Lyon', 
-        'Reims', 
-        'Lille', 
-        'Luxemburg', 
-        'Strasbourge', 
-        'Nancy',
-        'Geneve',
-        'Zurich',
-        'Frankfurt',
-        'Stuttgart',
-        'Bordeaux'
-    ],
-    'Lyon': [
-        'Saint-Etienne',
-        'Marseille',
-        'Geneve',
-        'Paris',
-        'Zurich',
-        'Grenoble',
-        'Montpellier'
-    ],
-    'Marseille': [
-        'Nice',
-        'Montpellier',
-        'Lyon',
-        'Grenoble',
-    ],
-    'Montpellier': [
-        'Barcelona',
-        'Marseille',
-        'Grenoble',
-        'Lyon'
-    ],
-    'Barcelona': [
-        'Montpellier',
-    ],
-    'Nice': [
-        'Marseille'
-    ],
-    'Grenoble': [
-        'Milano',
-        'Lyon'
-    ],
-    'Saint-Etienne': [
-        'Lyon'
-    ],
-    'Milano': [
-        'Grenoble'
-    ],
-    'Geneve': [
-        'Paris',
-        'Lyon',
-        'Zurich'
-    ],
-    'Zurich': [
-        'Paris',
-        'Lyon',
-        'Geneve'
-    ],
-    'Reims': [
-        'Paris',
-        'La Rochelle', 
-        'Strasbourge', 
-        'Nancy',
-        'Frankfurt',
-        'Stuttgart',
-        'Luxemburg'
-    ],
-    'Luxemburg': [
-        'Paris',
-        'Reims',
-        'Strasbourge',
-        'Nancy',
-        'Stuttgart',
-        'Frankfurt'
-    ],
-    'Nancy': [
-        'Luxemburg',
-        'Reims',
-        'Paris',
-        'Frankfurt',
-        'Stuttgart',
-        'Strasbourge'
-    ],
-    'Frankfurt': [
-        'Luxemburg',
-        'Reims',
-        'Paris',
-        'Stuttgart',
-        'Strasbourge',
-        'Nancy'
-    ],
-    'Stuttgart': [
-        'Luxemburg',
-        'Reims',
-        'Paris',
-        'Strasbourge',
-        'Nancy'
-        'Frankfurt'
-    ],
-    'Strasbourge': [
-        'Luxemburg',
-        'Reims',
-        'Paris',
-        'Colmar',
-        'Nancy'
-        'Frankfurt',
-        'Stuttgart'
-    ],
-    'Colmar': [
-        'Strasbourge'
-    ],
-    'Lille': [
-        'Paris',
-        'London',
-        'Bruxelles'
-    ],
-    'Bruxelles': [
-        'Amsterdam',
-        'Lille',
-        'Paris',
-        'London'
-    ],
-    'Amsterdam': [
-        'Bruxelles'
-    ],
-    'Rennes': [
-        'Paris',
-        'Nantes',
-        'La Rochelle',
-        'Bordeaux',
-        'Saint-Malo',
-        'Quimper',
-        'Brest',
-    ],
-    'Saint-Malo': [
-        'Rennes',
-        'Brest'
-    ],
-    'Brest': [
-        'Rennes',
-        'Saint-Malo'
-    ],
-    'Quimper': [
-        'Rennes'
-    ],
-    'Nantes': [
-        'Paris',
-        'Bordeaux',
-        'La Rochelle',
-        'Rennes'
-    ],
-    'Bordeaux': [
-        'Paris',
-        'Nantes',
-        'Rennes',
-        'La Rochelle',
-        'Hendaye',
-        'Pau',
-        'Toulouse',
-    ],
-    'Toulouse': [
-        'Bordeaux',
-        'Hendaye',
-        'Pau',
-    ],
-    'Pau': [
-        'Bordeaux',
-        'Hendaye',
-        'Toulouse',
-    ],
-    'Hendaye': [
-        'Bordeaux',
-        'Toulouse',
-        'Pau',
-    ]
-}
-G = nx.Graph(tgv_graph)
-nx.draw(G, with_labels=True)
-plt.show()
+def create_tgv_graph():
+    tgv_graph = {
+        'Paris': [
+            'London', 
+            'Bruxelles', 
+            'Nantes', 
+            'Rennes', 
+            'La Rochelle', 
+            'Lyon', 
+            'Reims', 
+            'Lille', 
+            'Luxemburg', 
+            'Strasbourge', 
+            'Nancy',
+            'Geneve',
+            'Zurich',
+            'Frankfurt',
+            'Stuttgart',
+            'Bordeaux'
+        ],
+        'Lyon': [
+            'Saint-Etienne',
+            'Marseille',
+            'Geneve',
+            'Paris',
+            'Zurich',
+            'Grenoble',
+            'Montpellier'
+        ],
+        'Marseille': [
+            'Nice',
+            'Montpellier',
+            'Lyon',
+            'Grenoble',
+        ],
+        'Montpellier': [
+            'Barcelona',
+            'Marseille',
+            'Grenoble',
+            'Lyon'
+        ],
+        'Barcelona': [
+            'Montpellier',
+        ],
+        'Nice': [
+            'Marseille'
+        ],
+        'Grenoble': [
+            'Milano',
+            'Lyon'
+        ],
+        'Saint-Etienne': [
+            'Lyon'
+        ],
+        'Milano': [
+            'Grenoble'
+        ],
+        'Geneve': [
+            'Paris',
+            'Lyon',
+            'Zurich'
+        ],
+        'Zurich': [
+            'Paris',
+            'Lyon',
+            'Geneve'
+        ],
+        'Reims': [
+            'Paris',
+            'La Rochelle', 
+            'Strasbourge', 
+            'Nancy',
+            'Frankfurt',
+            'Stuttgart',
+            'Luxemburg'
+        ],
+        'Luxemburg': [
+            'Paris',
+            'Reims',
+            'Strasbourge',
+            'Nancy',
+            'Stuttgart',
+            'Frankfurt'
+        ],
+        'Nancy': [
+            'Luxemburg',
+            'Reims',
+            'Paris',
+            'Frankfurt',
+            'Stuttgart',
+            'Strasbourge'
+        ],
+        'Frankfurt': [
+            'Luxemburg',
+            'Reims',
+            'Paris',
+            'Stuttgart',
+            'Strasbourge',
+            'Nancy'
+        ],
+        'Stuttgart': [
+            'Luxemburg',
+            'Reims',
+            'Paris',
+            'Strasbourge',
+            'Nancy'
+            'Frankfurt'
+        ],
+        'Strasbourge': [
+            'Luxemburg',
+            'Reims',
+            'Paris',
+            'Colmar',
+            'Nancy'
+            'Frankfurt',
+            'Stuttgart'
+        ],
+        'Colmar': [
+            'Strasbourge'
+        ],
+        'Lille': [
+            'Paris',
+            'London',
+            'Bruxelles'
+        ],
+        'Bruxelles': [
+            'Amsterdam',
+            'Lille',
+            'Paris',
+            'London'
+        ],
+        'Amsterdam': [
+            'Bruxelles'
+        ],
+        'Rennes': [
+            'Paris',
+            'Nantes',
+            'La Rochelle',
+            'Bordeaux',
+            'Saint-Malo',
+            'Quimper',
+            'Brest',
+        ],
+        'Saint-Malo': [
+            'Rennes',
+            'Brest'
+        ],
+        'Brest': [
+            'Rennes',
+            'Saint-Malo'
+        ],
+        'Quimper': [
+            'Rennes'
+        ],
+        'Nantes': [
+            'Paris',
+            'Bordeaux',
+            'La Rochelle',
+            'Rennes'
+        ],
+        'Bordeaux': [
+            'Paris',
+            'Nantes',
+            'Rennes',
+            'La Rochelle',
+            'Hendaye',
+            'Pau',
+            'Toulouse',
+        ],
+        'Toulouse': [
+            'Bordeaux',
+            'Hendaye',
+            'Pau',
+        ],
+        'Pau': [
+            'Bordeaux',
+            'Hendaye',
+            'Toulouse',
+        ],
+        'Hendaye': [
+            'Bordeaux',
+            'Toulouse',
+            'Pau',
+        ]
+    }
+
+    return nx.Graph(tgv_graph)
+
+def visualize_graph(G):
+    nx.draw(G, with_labels=True)
+    plt.show()
+
+def get_vertices_qty(G):
+    return G.number_of_nodes()
+
+def get_edges_qty(G):
+    return G.number_of_edges()
+
+def get_vertices_degrees(G):
+    return G.degree()
+
+G = create_tgv_graph()
+print(f'Vertices quantity in the graph: {get_vertices_qty(G)}')
+print(f'Edges quantity in the graph: {get_edges_qty(G)}')
+for vertex in get_vertices_degrees(G):
+    print(f'Vertex {vertex[0]} has degree {vertex[1]}')
+visualize_graph(G)
